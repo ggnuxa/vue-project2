7b9e9e7372c1411b11b041154e47d7b74c144eb6
Vladimir Ustimov <ggnuxa@gmail.com>
2020-01-13 13:56:30 +0200

    prep

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  globals: {
+    $: false,
+    jQuery: false
   }
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavbarComponent></NavbarComponent>
+
+    <SidebarComponent></SidebarComponent>
+
+    <ContentComponent></ContentComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContentComponent from './components/Content.vue';
+import SidebarComponent from './components/Sidebar.vue';
+import NavbarComponent from './components/Navbar.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ContentComponent,
+    SidebarComponent,
+    NavbarComponent
+  },
+  mounted() {
+    // $(document).ready(function() {
+    //   $('.dropdown-button').dropdown({
+    //     constrainWidth: false,
+    //     hover: true,
+    //     belowOrigin: true,
+    //     alignment: 'left'
+    //   });
+    //   // JAVASCRIPT START HERE //
+    //   $('.button-menu').sideNav({
+    //     menuWidth: 300,
+    //     edge: 'left',
+    //     closeOnClick: false,
+    //     draggable: true,
+    //     onOpen: function() {
+    //       // console.log('Menu Open');
+    //     },
+    //     onClose: function() {
+    //       // console.log('Menu Close');
+    //     }
+    //   });
+    // });
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
