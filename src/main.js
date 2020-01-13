3cf61cec84bfe43482132bcc0925414f475406be
Vladimir Ustimov <ggnuxa@gmail.com>
2020-01-13 14:24:25 +0200

    sfsdf

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import 'materialize-css/dist/js/materializedfgdf.js';
+import 'materialize-css/dist/js/materialize.js';
 import 'materialize-css/dist/css/materialize.min.css';
 
 Vue.config.productionTip = false;
